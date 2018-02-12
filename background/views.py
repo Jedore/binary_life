a2845e5ee8aa946ab28b2aeabab6853f5cbcbbbb
jedore <cui_baochang_ext@wuxiapptec.com>
2018-02-13 00:13:34 +0800

    optimize foreground and background and markdown

--- background/views.py
+++ background/views.py
@@ -6,6 +6,7 @@ from django.views import View
 
 from common.models import Article
 from common.models import ArticleType
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
@@ -42,11 +43,28 @@ class ArticleTypeView(View):
         ArticleType.objects.create(name=article_type)
         return JsonResponse({"ret": "SUCCESS"})
 
+    def delete(self, request):
+        data = json.loads(request.body)
+        type_id = data.get("typeId", 0)
+        try:
+            article_type = ArticleType.objects.get(id=type_id)
+            article_type.delete()
+            return JsonResponse({"ret_code": 0})
+        except ObjectDoesNotExist:
+            return JsonResponse({"ret_code": 1, "error": "ObjectDoesNotExist"})
+
 
 class ArticleView(View):
     def get(self, request):
         articles = Article.objects.all().order_by('-create_time')
         return render(request, 'background/article.html', locals())
 
-    def post(self, request):
-        pass
+    def delete(self, request):
+        data = json.loads(request.body)
+        article_id = data.get("articleId", 0)
+        try:
+            article = Article.objects.get(id=article_id)
+            article.delete()
+            return JsonResponse({"ret_code": 0})
+        except ObjectDoesNotExist as e:
+            return JsonResponse({"ret_code": 1, "error": "ObjectDoesNotExist"})
--- foreground/views.py
+++ foreground/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import get_object_or_404
 
 from common.models import Article
 from common.models import ArticleType
@@ -7,7 +9,22 @@ from common.models import ArticleType
 # Create your views here.
 
 
+@require_http_methods(["GET"])
 def index(request):
     articles = Article.objects.all().order_by("-create_time")
     article_types = ArticleType.objects.all().order_by("id")
     return render(request, 'foreground/index.html', locals())
+
+
+@require_http_methods(["GET"])
+def article(request, article_id):
+    article_types = ArticleType.objects.all().order_by("id")
+    _article = get_object_or_404(Article, id=article_id)
+    return render(request, "foreground/article.html", locals())
+
+
+@require_http_methods(["GET"])
+def type_article(request, type_id):
+    article_types = ArticleType.objects.all().order_by("id")
+    articles = Article.objects.filter(article_type=type_id)
+    return render(request, "foreground/index.html", locals())

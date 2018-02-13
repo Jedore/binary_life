dac7a6c8f864fd36e9bab92c1606a3de60f62ee4
jedore <cui_baochang_ext@wuxiapptec.com>
2018-02-13 14:08:30 +0800

    record views

--- background/urls.py
+++ background/urls.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.views import login_required
 from django.urls import path
+
 from .views import *
 
 urlpatterns = [
-    path('', BackgroundView.as_view()),
-    path('publish/', PublishView.as_view()),
-    path('article_type/', ArticleTypeView.as_view()),
-    path('article/', ArticleView.as_view()),
+    path('', login_required(BackgroundView.as_view())),
+    path('publish/', login_required(PublishView.as_view())),
+    path('article_type/', login_required(ArticleTypeView.as_view())),
+    path('article/', login_required(ArticleView.as_view())),
 ]
--- foreground/views.py
+++ foreground/views.py
@@ -1,14 +1,16 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-from django.shortcuts import get_object_or_404
 
 from common.models import Article
 from common.models import ArticleType
+from common.utils import add_total_views
 
 
 # Create your views here.
 
 
+@add_total_views
 @require_http_methods(["GET"])
 def index(request):
     articles = Article.objects.all().order_by("-create_time")
@@ -16,13 +18,17 @@ def index(request):
     return render(request, 'foreground/index.html', locals())
 
 
+@add_total_views
 @require_http_methods(["GET"])
 def article(request, article_id):
     article_types = ArticleType.objects.all().order_by("id")
     _article = get_object_or_404(Article, id=article_id)
+    _article.views = _article.views + 1
+    _article.save()
     return render(request, "foreground/article.html", locals())
 
 
+@add_total_views
 @require_http_methods(["GET"])
 def type_article(request, type_id):
     article_types = ArticleType.objects.all().order_by("id")

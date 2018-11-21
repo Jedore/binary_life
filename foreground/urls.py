31c342c1f6ef6911e0502adb7b25b07cf40b0d9a
jedore-JTCE <jedorefight@gmail.com>
2018-11-21 10:54:10 +0800

    use re_path for processing url with anchor tag

--- foreground/urls.py
+++ foreground/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import *
 
 app_name = 'foreground'
 urlpatterns = [
-    path("article/<int:article_id>", article, name='article'),
+    re_path(r"article/(?P<article_id>\d+)/(#.+)?", article, name='article'),
     path("type_articles/<int:type_id>", type_articles, name='type_articles'),
     path("tag_articles/<int:tag_id>", tag_articles, name='tag_articles'),
     path("commit_comment/", commit_comment, name='comment'),
